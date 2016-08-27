3473c4628ec15698a96e1f4e21222d54a1b1cd7f
Eric Ricketts <eric_ricketts@me.com>
2016-08-26 21:36:44 -0400

    exercise 2 part a

--- IntroductionToProgramming/basics/spec/basics_spec.rb
+++ IntroductionToProgramming/basics/spec/basics_spec.rb
@@ -5,5 +5,11 @@ describe 'Execises for Basics Chapter of Introduction to Programming' do
     last_name = 'Ricketts'
     expect(first_name + ' ' + last_name).to eq('Eric Ricketts')
   end
-  
+
+  it 'exercise 2 part a' do
+    number = 5432
+    expect(number / 1000).to eq(5)         
+  end
+
+
 end
