b3df84b1d5c03848ab42f991ae39d6a4e35d3673
Eric Ricketts <eric_ricketts@me.com>
2016-08-26 21:28:02 -0400

    set up Rspec to work with exercises, it workedgit status

---
+++ IntroductionToProgramming/basics/spec/basics_spec.rb
@@ -0,0 +1,9 @@
+describe 'Execises for Basics Chapter of Introduction to Programming' do
+
+  it 'exercise 1' do
+    first_name = 'Eric'   
+    last_name = 'Ricketts'
+    expect(first_name + ' ' + last_name).to eq('Eric Ricketts')
+  end
+  
+end
