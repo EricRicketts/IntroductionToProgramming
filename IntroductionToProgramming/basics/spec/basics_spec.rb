b80d0b5b6c29db49a95f44055a3b6f6e96473f67
Eric Ricketts <eric_ricketts@me.com>
2016-08-26 21:49:10 -0400

    basics exercise 2

--- IntroductionToProgramming/basics/spec/basics_spec.rb
+++ IntroductionToProgramming/basics/spec/basics_spec.rb
@@ -1,15 +1,30 @@
 describe 'Execises for Basics Chapter of Introduction to Programming' do
 
+  before(:all) do
+    @number = 5432
+  end
+
   it 'exercise 1' do
     first_name = 'Eric'   
     last_name = 'Ricketts'
-    expect(first_name + ' ' + last_name).to eq('Eric Ricketts')
+    expect(first_name + ' ' + last_name).to eq 'Eric Ricketts'
   end
 
   it 'exercise 2 part a' do
     number = 5432
-    expect(number / 1000).to eq(5)         
+    expect(@number / 1000).to eq 5         
   end
 
+  it 'exercise 2 part b' do
+    expect(@number % 1000 / 100).to eq 4     
+  end
+
+  it 'exercise 2 part c' do
+    expect(@number % 100 / 10).to eq 3
+  end
+
+  it 'exercise 2 part d' do
+    expect(@number % 10).to eq 2
+  end
 
 end
