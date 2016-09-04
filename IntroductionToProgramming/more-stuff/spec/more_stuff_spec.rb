d8494615df2c4a6ed92609e98c5cc2b2dc0a50a8
Eric Ricketts <eric_ricketts@me.com>
2016-09-03 20:45:09 -0400

    more stuff chapter

--- IntroductionToProgramming/more-stuff/spec/more_stuff_spec.rb
+++ IntroductionToProgramming/more-stuff/spec/more_stuff_spec.rb
@@ -23,4 +23,25 @@ describe 'Introduction To Programming More Stuff Exercises' do
     end
 
   end
+
+
 end
+
+def execute(&block)
+  block
+end
+puts "Second Exercise"
+execute { puts "Hello from inside the execute method!" }
+puts "nothing is printed for the second exercise because the block is not called from within the method"
+puts "in order for it to work the block needs the call method, ie, block.call from within execture"
+puts
+puts "Third Exercise"
+puts "Exception handling deals with errors in a predictable and manageable way"
+puts "it can do so by altering the flow of control to an option which does not cause the program to stop"
+puts
+puts "Fourth Exercise"
+puts "Answered in the exercise 2 comments"
+puts
+puts "Fifth Exercise"
+puts "The block parameter needs an ampersand in front of it, otherwise the method treats it as a non-block parameter"
+puts "and only block objects have the call method associated with them"
