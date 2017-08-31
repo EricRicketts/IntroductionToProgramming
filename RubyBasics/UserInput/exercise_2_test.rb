67d8b6d6fd3df6e1cf11839ded6b7c90ce3a5f86
EricRicketts <eric_ricketts@me.com>
2017-08-31 06:19:25 -0400

    modifications to exercise 2 to accommodate non-integer input

--- RubyBasics/UserInput/exercise_2_test.rb
+++ RubyBasics/UserInput/exercise_2_test.rb
@@ -6,13 +6,23 @@ class Exercise2Test < Minitest::Test
   def setup
     def age
       puts ">> What is your age in years?"
-      age_in_years = gets.chomp.strip.to_i
-      puts "You are #{age_in_years*12} months old."
+      age_in_years = gets.chomp.strip
+      age_in_months = age_in_years.to_i * 12
+      error_string = "Sorry, integer only input required."
+      valid_string = "You are #{age_in_months} months old."
+      output_string = /\A\d+\z/ === age_in_years ? valid_string : error_string
+      puts output_string
     end
   end
 
-  def test_age
+  def test_valid_age
     $stdin = StringIO.new("  35  \n")
     assert_output(/\A>> What is your age in years\?\nYou are 420 months old\.\n\z/) { age }
   end
+
+  def test_invalid_age
+    $stdin = StringIO.new(" 4rr \n")   
+    assert_output(/\A>> What is your age in years\?\nSorry, integer only input required\.\n\z/) { age }
+  end
+
 end
