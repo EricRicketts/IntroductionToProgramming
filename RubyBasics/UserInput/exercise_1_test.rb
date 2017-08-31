930340e69ab66ae1f75b29e131a739edd1117989
EricRicketts <eric_ricketts@me.com>
2017-08-31 05:27:20 -0400

    I forgot to add the introductory output string

--- RubyBasics/UserInput/exercise_1_test.rb
+++ RubyBasics/UserInput/exercise_1_test.rb
@@ -4,13 +4,14 @@ require 'minitest/pride'
 class ExerciseOne < Minitest::Test
   def setup
     def repeat_after_me
+      puts ">> Type anything you want:"
       puts gets.chomp
     end
   end
 
   def test_repeat_after_me
     $stdin = StringIO.new("my input string\n")
-    assert_output(/my input string/) { repeat_after_me } 
+    assert_output(/\A>> Type anything you want:\nmy input string\n\z/) { repeat_after_me } 
     $stdin.rewind
     $stdin = STDIN   
   end
