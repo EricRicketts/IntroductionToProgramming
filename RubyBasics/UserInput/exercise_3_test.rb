4404b74c39a1ed3481ed6fec255e115ef1cc1c1e
EricRicketts <eric_ricketts@me.com>
2017-09-02 12:56:33 -0400

    got exercise 3 partially workign I have to add another test case

---
+++ RubyBasics/UserInput/exercise_3_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class Exercise3Test < Minitest::Test
+  
+  def setup
+    def something
+      output = %w(something help conversation drill plateau up down left right sea sky).sample
+      puts "Do you want me to print something? (y/n)"
+      user_response = gets.chomp.strip
+      puts /\A[yYeEsS]+\z/.match(user_response) ? output : ""
+    end
+  end
+
+  def teardown
+    $stdin.rewind
+    $stdin = STDIN
+  end
+
+  def test_valid_input_yes
+    $stdin = StringIO.new("y\n")
+    possible_matches = "something|help|conversation|drill|plateau|up|down|left|right|sea|sky"
+    assert_output(/\ADo you want me to print something\? \(y\/n\)\n#{possible_matches}\n/) { something }
+  end
+
+  def test_valid_input_no
+    $stdin = StringIO.new("n\n")
+    assert_output(/\ADo you want me to print something\? \(y\/n\)\n\n/) { something }    
+  end
+
+end
