98db906a5ce785089bb4ee978bf3482b27149f19
EricRicketts <eric_ricketts@me.com>
2017-09-02 13:06:03 -0400

    exercise 3 complete

--- RubyBasics/UserInput/exercise_3_test.rb
+++ RubyBasics/UserInput/exercise_3_test.rb
@@ -8,7 +8,7 @@ class Exercise3Test < Minitest::Test
       output = %w(something help conversation drill plateau up down left right sea sky).sample
       puts "Do you want me to print something? (y/n)"
       user_response = gets.chomp.strip
-      puts /\A[yYeEsS]+\z/.match(user_response) ? output : ""
+      puts (/\A[yYeEsS]+\z/.match(user_response) ? output : "")
     end
   end
 
@@ -17,14 +17,25 @@ class Exercise3Test < Minitest::Test
     $stdin = STDIN
   end
 
-  def test_valid_input_yes
+  def test_valid_input_just_y
     $stdin = StringIO.new("y\n")
     possible_matches = "something|help|conversation|drill|plateau|up|down|left|right|sea|sky"
     assert_output(/\ADo you want me to print something\? \(y\/n\)\n#{possible_matches}\n/) { something }
   end
 
-  def test_valid_input_no
+  def test_valid_input_yes
+    $stdin = StringIO.new("  yES  \n")
+    possible_matches = "something|help|conversation|drill|plateau|up|down|left|right|sea|sky"
+    assert_output(/\ADo you want me to print something\? \(y\/n\)\n#{possible_matches}\n/) { something }
+  end
+
+  def test_valid_input_just_n
     $stdin = StringIO.new("n\n")
+    assert_output(/\ADo you want me to print something\? \(y\/n\)\n\n/) { something }    
+  end
+
+  def test_valid_input_other_than_n
+    $stdin = StringIO.new("  help  \n")
     assert_output(/\ADo you want me to print something\? \(y\/n\)\n\n/) { something }    
   end
 
