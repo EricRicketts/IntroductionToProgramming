fed0eb0f74e1f777a824517d1c22299d0ad3d338
EricRicketts <eric_ricketts@me.com>
2017-09-02 18:04:54 -0400

    exercise 5 now completed

--- RubyBasics/UserInput/exercise_5_test.rb
+++ RubyBasics/UserInput/exercise_5_test.rb
@@ -36,4 +36,16 @@ class Exercise5Test < Minitest::Test
     assert_output(/#{expected_output}/) { launch_school_printer }
   end
 
+  def test_non_integer_input
+    expected_output = "That\'s not enough lines\."
+    $stdin = StringIO.new("n3u\n3\n")
+    assert_output(/#{expected_output}/) { launch_school_printer }    
+  end
+
+  def test_greater_than_minimum_input
+    expected_output = "Launch School is the best!\n" * 4
+    $stdin = StringIO.new("4\n")
+    assert_output(/#{expected_output}/) { launch_school_printer }    
+  end
+
 end
