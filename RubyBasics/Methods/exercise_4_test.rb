3162531bb3330b500bf8f17b643ed2b39f19d9b6
EricRicketts <eric_ricketts@me.com>
2017-09-06 05:37:06 -0400

    exercise 4 Methods

---
+++ RubyBasics/Methods/exercise_4_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class Exercise4Test < Minitest::Test
+  
+  def setup
+    
+    def time_of_day(daylight)
+      daylight ? "It's daytime!" : "It's nighttime!"
+    end
+
+  end
+
+  def test_daytime
+    daylight = true
+    assert_equal(time_of_day(daylight), "It's daytime!")    
+  end
+
+  def test_nighttime
+    daylight = false
+    assert_equal(time_of_day(daylight), "It's nighttime!")
+  end
+
+  def test_either_day_or_night
+    daylight = [true, false].sample
+    assert_includes(["It's daytime!", "It's nighttime!"], time_of_day(daylight))
+  end
+end
