770b5684fdd7d066f5f9431d3e6723a80c33eb76
EricRicketts <eric_ricketts@me.com>
2017-09-07 05:31:09 -0400

    Methods exercise 8

---
+++ RubyBasics/Methods/exercise_8_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class Exercise8Test < Minitest::Test
+
+  NAMES = ['Dave', 'Sally', 'George', 'Jessica']
+  ACTIVITIES = ['walking', 'running', 'cycling']
+  
+  def setup
+    
+    def random_name(names)
+      names.sample
+    end
+
+    def random_activity(activities)
+      activities.sample
+    end
+
+    def sentence(a_name, an_activity)
+      a_name + " " + "went" + " " + an_activity + " " + "today."
+    end
+
+  end
+
+  def test_doing_something
+    assert_match(/\A\w+\swent\s\w+\stoday\./, sentence(random_name(NAMES), random_activity(ACTIVITIES)))
+  end
+
+end
